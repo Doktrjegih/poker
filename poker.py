24bbde6206db7492b30abbcc180bc45e5ac2f268
Doktrjegih <52949561+Doktrjegih@users.noreply.github.com>
2021-01-04 23:15:45 +0500

    можно откладывать карты в список

--- poker.py
+++ poker.py
@@ -1,14 +1,29 @@
 import random
 
+
 class Card():
     def __init__(self):
         self.t = random.randint(2, 15)
-    
-    def array(self):
-        a = []
-        for i in range(5):
-            a.append(self.t)
-        return a
 
-b = Card()
-print(b.array())
+    def ne_init(self):
+        return self.t
+
+table = []
+for i in range(5):
+    a = Card()
+    c = a.ne_init()
+    table.append(c)
+print('стол', table)
+hand = []
+for i in range(2):
+    a = Card()
+    c = a.ne_init()
+    hand.append(c)
+print('рука', hand)
+all = table + hand
+print('все карты', all)
+choise = input('откладываем')
+arr_choise = []
+for i in choise:
+    arr_choise.append(all[int(i) - 1])
+print(arr_choise)
