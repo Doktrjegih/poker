9684cb1b32e9e0550a545f0a237abb4e88e349e2
Doktrjegih <doktrjegih@gmail.com>
2021-01-05 12:17:05 +0500

    проверки на 2,3,4

--- poker.py
+++ poker.py
@@ -8,6 +8,20 @@ class Card():
     def ne_init(self):
         return self.t
 
+
+def combo(cards):
+    counter = 1
+    for i in range(len(cards) - 1):
+        if cards[i] == cards[i + 1]:
+            counter += 1
+    if counter == 2:
+        print('пара')
+    elif counter == 3:
+        print('тройка')
+    elif counter == 4:
+        print('каре')
+
+
 table = []
 for i in range(5):
     a = Card()
@@ -20,10 +34,12 @@ for i in range(2):
     c = a.ne_init()
     hand.append(c)
 print('рука', hand)
-all = table + hand
+# all = table + hand
+all = [2, 2, 2, 2, 3, 4, 5]  # для тестов
 print('все карты', all)
-choise = input('откладываем')
+choise = input('откладываем\n')
 arr_choise = []
 for i in choise:
     arr_choise.append(all[int(i) - 1])
 print(arr_choise)
+combo(arr_choise)
