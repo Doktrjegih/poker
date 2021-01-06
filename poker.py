2613976ad693e1363eb0771246611583b9eb4954
Doktrjegih <52949561+Doktrjegih@users.noreply.github.com>
2021-01-06 23:35:31 +0500

    криво добавил масти

--- poker.py
+++ poker.py
@@ -4,15 +4,16 @@ import random
 class Card():
     def __init__(self):
         self.t = random.randint(2, 15)
+        self.suit = random.choice(suit)
 
     def ne_init(self):
-        return self.t
+        return self.t, self.suit
 
 
 def combo(cards):
     counter = 1
     for i in range(len(cards) - 1):
-        if cards[i] == cards[i + 1]:
+        if cards[i][0] == cards[i + 1][0]:
             counter += 1
     if counter == 2:
         print('пара')
@@ -22,6 +23,7 @@ def combo(cards):
         print('каре')
 
 
+suit = ['spades', 'hearts', 'diamonds', 'clubs']
 table = []
 for i in range(5):
     a = Card()
@@ -34,8 +36,8 @@ for i in range(2):
     c = a.ne_init()
     hand.append(c)
 print('рука', hand)
-# all = table + hand
-all = [2, 2, 2, 2, 3, 4, 5]  # для тестов
+all = table + hand
+# all = [2, 2, 2, 2, 3, 4, 5]  # для тестов
 print('все карты', all)
 choise = input('откладываем\n')
 arr_choise = []
